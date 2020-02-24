dc414913a3998d84a40386b89690e4205fe0d091
Paul Maguire <pewm97@gmail.com>
2020-02-24 21:14:13 +0000

    second commit
    Signed-off-by: Paul Maguire <pewm97@gmail.com>

--- app/src/main/java/com/example/gaa/startGameContinue.java
+++ app/src/main/java/com/example/gaa/startGameContinue.java
@@ -3,21 +3,30 @@ package com.example.gaa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class startGameContinue extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class startGameContinue extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
     private Button mStartButton;
     private Button mPauseButton;
     private Button mResetButton;
     private Chronometer mChronometer;
-
     private long lastPause;
+
+    //Score Var
+    double scoreHomeTeam = 0;
+    double scoreAwayTeam = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +39,23 @@ public class startGameContinue extends AppCompatActivity {
         mChronometer = (Chronometer) findViewById(R.id.chronometer);
 
 
-
-        mStartButton.setOnClickListener(new View.OnClickListener(){
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(lastPause != 0) {
+                if (lastPause != 0) {
                     mChronometer.setBase(mChronometer.getBase() + SystemClock.elapsedRealtime() - lastPause);
+                } else {
+                    mChronometer.setBase(SystemClock.elapsedRealtime());
                 }
-                    else{ mChronometer.setBase(SystemClock.elapsedRealtime());
-                }
-                    mChronometer.start();
-                    mStartButton.setEnabled(false);
-                    mStartButton.setEnabled(true);
+                mChronometer.start();
+                mStartButton.setEnabled(false);
+                mStartButton.setEnabled(true);
             }
 
         });
 
-        mPauseButton.setOnClickListener(new View.OnClickListener(){
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 lastPause = SystemClock.elapsedRealtime();
@@ -58,7 +66,7 @@ public class startGameContinue extends AppCompatActivity {
             }
 
         });
-        mResetButton.setOnClickListener(new View.OnClickListener(){
+        mResetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mChronometer.stop();
@@ -72,4 +80,83 @@ public class startGameContinue extends AppCompatActivity {
 
     }
 
+    //Pop up function
+    public void inputStats(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.inputstats_menu);
+        popup.show();
+
+    }
+
+
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Point:
+                Toast.makeText(this, "Point Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.Goal:
+                Toast.makeText(this, "Goal Selected", Toast.LENGTH_SHORT);
+                return true;
+                case R.id.pointConceded:
+                Toast.makeText(this, "Point Conceded", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.goalConceded:
+                Toast.makeText(this, "Goal Conceded", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.yellowCard:
+                Toast.makeText(this, "Yellow Card Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.redCard:
+                Toast.makeText(this, "Red card Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.wide:
+                Toast.makeText(this, "Wide Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.freeWon:
+                Toast.makeText(this, "Free won Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.foul:
+                Toast.makeText(this, "Foul Selected", Toast.LENGTH_SHORT);
+                return true;
+            case R.id.mark:
+                Toast.makeText(this, "Mark Selected", Toast.LENGTH_SHORT);
+                return true;
+                default:
+                    return false;
+        }
+    }
+
+    public void homeTeamScoresGoal (MenuItem item){
+        scoreHomeTeam = scoreHomeTeam + 1;
+        displayforHomeTeam(scoreHomeTeam);
+    }
+
+    public void homeTeamScoresPoint(View view){
+        scoreHomeTeam = scoreHomeTeam + 0.01;
+        displayforHomeTeam(scoreHomeTeam);
+    }
+    private void displayforHomeTeam(double scoreHomeTeam){
+        TextView scoreView= (TextView) findViewById(R.id.homeTeamScore);
+        scoreView.setText("" + scoreHomeTeam);
+
+    }
+    //Scoring Away function
+    public void awayTeamScoresGoal(View view){
+        scoreAwayTeam = scoreAwayTeam + 1;
+        displayforAwayTeam(scoreAwayTeam);
+    }
+    public void awayTeamScoresPoint(View view){
+        scoreAwayTeam = scoreAwayTeam + 0.01;
+        displayforAwayTeam(scoreAwayTeam);
+    }
+    private void displayforAwayTeam(double scoreHomeTeam){
+        TextView scoreView= (TextView) findViewById(R.id.awayTeamScore);
+        scoreView.setText("" + scoreAwayTeam);
+
+    }
+
 }
+
+
+
